6f286f407cd1accc23d0a192c91884af3a485bc8
Serge Mazille <serge.mazille@gmail.com>
2021-09-21 11:25:34 +0200

    Manage functions returning properties

--- tests/support/WrapperBuilder.ts
+++ tests/support/WrapperBuilder.ts
@@ -3,54 +3,101 @@ import { Wrapper, createLocalVue, shallowMount } from '@vue/test-utils';
 import Vue from 'vue';
 
 export default class WrapperBuilder<T extends Vue> {
-  data: any = () => ({});
-  propsData: any = {};
-  mocks: any = {};
-  provide: any = {};
+  data?: Function = () => ({});
+  propsData?: any = {};
+  mocks?: any = {};
+  provide?: any = {};
 
   constructor(private readonly ComponentVue: Record<string, any>, defaults?: Record<string, any>) {
     if (defaults) this.set(defaults);
   }
 
-  build(params?: Partial<Record<keyof WrapperBuilder<T>, any>>): Wrapper<T, Element> {
-    if (params) this.set(params);
+  build(builderFields?: Partial<WrapperBuilder<T>>): Wrapper<T, Element> {
+    if (builderFields) this.set(builderFields);
 
     return this.createWrapper();
   }
 
-  private set(params: Record<string, any>) {
-    this.data = params.data || this.data;
-    this.provide = params.provide ? this.updateProvideWith(params.provide) : this.provide;
-
-    Object.entries(params)
-      .filter(([key]) => key !== 'data' && key !== 'provide')
-      .forEach(param => {
-        const [key, value] = param as [keyof WrapperBuilder<T>, Record<string, any> | Function];
-
-        this[key] = {
-          ...this[key],
-          ...value,
-        };
-      });
+  private set(builderFields: Partial<WrapperBuilder<T>>) {
+    this.assertProvideFieldIsValid(builderFields.provide);
+    this.updateAllFieldsWith(builderFields);
   }
 
-  private updateProvideWith(provideParam: Record<string, any>) {
-    return Object.entries(provideParam).reduce((acc, [propertyKey, propertyValue]) => {
-      const doesComponentHaveProperty = propertyKey in this.ComponentVue.options.inject;
+  private assertProvideFieldIsValid(provideField?: Record<string, object | Function>) {
+    if (!provideField) return;
 
-      if (!doesComponentHaveProperty) {
-        throw new Error(`No property nammed "${propertyKey}" can be injected into "${this.ComponentVue.name}". Please check the spelling.`);
+    Object.keys(provideField).forEach(propertyKey => {
+      const doesComponentOwnProperty = propertyKey in this.ComponentVue.options.inject;
+
+      if (!doesComponentOwnProperty) {
+        throw new Error(
+          `No property named "${propertyKey}" can be injected into "${this.ComponentVue.name}". Please check the spelling of this property.`
+        );
+      }
+    });
+  }
+
+  private updateAllFieldsWith(builderFields: Partial<WrapperBuilder<T>>) {
+    Object.entries(builderFields).forEach(field => {
+      const [fieldKey, fieldValue] = field as [keyof WrapperBuilder<T>, Record<string, any> | Function];
+      const existingFieldValue = this[fieldKey];
+
+      this[fieldKey] = this.updateFieldWith(fieldValue, existingFieldValue);
+    });
+  }
+
+  private updateFieldWith(
+    newField: Record<string, object | Function> | Function = {},
+    existingField: Record<string, object | Function> = {}
+  ) {
+    // most certainly "data" field
+    if (typeof newField === 'function') {
+      const existingFunction = (<unknown>existingField) as Function;
+
+      return this.mergeFunctionReturningProperties(newField, existingFunction);
+    }
+
+    // other fields: propsData, provide, mocks
+    const fieldProperties = Object.entries(newField);
+
+    return fieldProperties.reduce((fieldAcc, [propertyKey, propertyValue]) => {
+      const existingValue = fieldAcc[propertyKey];
+
+      switch (typeof propertyValue) {
+        case 'function':
+          fieldAcc[propertyKey] = this.mergeFunctions(propertyValue, existingValue as Function);
+          break;
+
+        // property composed of multiple values
+        case 'object':
+          fieldAcc[propertyKey] = {
+            ...existingValue,
+            ...propertyValue,
+          };
+          break;
+
+        // value is a simple primitive
+        default:
+          fieldAcc[propertyKey] = propertyValue;
+          break;
       }
 
-      const updatedPropertyValue = {
-        ...acc[propertyKey],
-        ...propertyValue,
-      };
+      return fieldAcc;
+    }, existingField);
+  }
 
-      acc[propertyKey] = typeof propertyValue === 'function' ? propertyValue : updatedPropertyValue;
+  private mergeFunctionReturningProperties(newFunction?: Function, existingFunction?: Function) {
+    return () => ({
+      ...(existingFunction && existingFunction()),
+      ...(newFunction && newFunction()),
+    });
+  }
 
-      return acc;
-    }, this.provide);
+  private mergeFunctions(newFunction: Function, existingFunction: Function) {
+    const getProperties = this.mergeFunctionReturningProperties(newFunction, existingFunction);
+    const doesReturnProperties = Object.keys(getProperties()).length;
+
+    return doesReturnProperties ? getProperties : newFunction;
   }
 
   private createWrapper() {
